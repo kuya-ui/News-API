baf0ea6f78c25d7ef4ef2fad4dcb7d3b0d5ae40a
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-16 17:59:26 +0300

    changes

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
 
     NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}&language=en'
     # https://newsapi.org/v2/top-headlines/sources?apiKey={}
+    # https://newsapi.org/v2/sources?apiKey={}&language=en
     ARTICLES_BASE_URL =  'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = '8120360ba9e342dbaccb75e01109ca34'
     # SECRET_KEY = os.environ.get('SECRET_KEY')
